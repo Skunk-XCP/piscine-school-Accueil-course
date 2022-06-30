fb08be436d93518b4a246c2c8af3759ffdae8021
unknown <skunk-xcp@hotmail.fr>
2022-06-30 14:30:25 +0200

    test

---
+++ website-skeleton/src/Controller/AccueilController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller
+{
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+    class AccueilController
+    {
+
+        /**
+         * @Route("/accueil", name="accueil")
+         */
+        public function accueil()
+        {
+            return new Response("Bonjour");
+        }
+    }
+}
